2c8d773e41c1961cfced7ec81e5b2bfeaca672b6
Katrina H. Turner <46706381+KatrinaTurner@users.noreply.github.com>
2022-09-12 13:05:08 -1000

    add checker for Field Options

--- src/esnetChord.tsx
+++ src/esnetChord.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {PanelProps} from '@grafana/data';
 import {ChordOptions} from 'types';
+import {useTheme2} from '@grafana/ui';
+
 // import * as d3 from 'd3';
 
 import * as chord from './chord.js';
@@ -14,15 +16,29 @@ export const esnetChord: React.FC<Props> = ({
   height,
   id,
 }) => {
-  const ref = chord.chord(
-      data,
-      options.sourceField,
-      options.targetField,
-      options.valueField,
-      height,
-      options.txtLength,
-      options.colorBySource,
-      options.pointLength,
-  );
-  return <svg ref={ref} width={width} height={height}></svg>;
+  if (options.sourceField && options.targetField && options.valueField) {
+    console.log(options.sourceField, options.targetField, options.valueField);
+    const ref = chord.chord(
+        data,
+        options.sourceField,
+        options.targetField,
+        options.valueField,
+        height,
+        options.txtLength,
+        options.colorBySource,
+        options.pointLength,
+    );
+    return <svg ref={ref} width={width} height={height}></svg>;
+  } else {
+    const theme = useTheme2();
+    console.log('missing field options');
+    const text = 'Please set Source, Target and Value Field Options';
+    return (
+      <svg width={width} height={height}>
+        <text x="0" y="15" fill={theme.colors.text.primary}>
+          {text}
+        </text>
+      </svg>
+    );
+  }
 };
